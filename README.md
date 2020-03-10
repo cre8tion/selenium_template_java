db71e654c0c2899c0e4ef0220fa4255c409d3c34
Cre8tion <ljl1997@gmail.com>
2020-03-10 22:02:28 +0800

    Initialised Template

---
+++ README.md
@@ -0,0 +1,5 @@
+# Template for Selenium Testing in Java
+
+Selenium-server and Firefox driver is already included for easy access.
+
+Just open in a IDE such as IntelliJ and it will work.
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Main {
+
+    public static void main(String[] args) {
+      FirefoxDriver driver = new FirefoxDriver();
+      driver.navigate().to("https://www.firefox.com");
+      driver.close();
+    }
+}
